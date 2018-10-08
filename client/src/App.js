d931fd45ffe620f5f511741502abd8fbc0e196eb
Jeff Maxim <frey.maxim@gmail.com>
2018-10-07 21:32:10 -0400

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import NotFound from './NotFound'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  render () {
+    return <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   }
 }
 
-export default App;
+export default App
